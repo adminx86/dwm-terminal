9451d1685f3e39b2d51fc73ea15117362212c008
adminx86 <150718558+adminx86@users.noreply.github.com>
2023-11-13 23:29:46 +0530

    Create client.py

---
+++ cn/client.py
@@ -0,0 +1,24 @@
+import socket
+
+
+def run_client():
+  client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+  server_ip = "127.0.0.1"
+  server_port = 8080
+  client.connect((server_ip, server_port))
+
+  while True:
+    msg = input("Enter message: ")
+    client.send(msg.encode("utf-8")[:1024])
+    response = client.recv(1024)
+    response = response.decode("utf-8")
+    if msg == "closed":
+      break
+
+    print(f"Received: {response}")
+    
+  client.close()
+  print("Connection to server closed")
+
+run_client()
